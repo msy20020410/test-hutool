0649e8b66a94bbd666d38c269c0876ab79e9a386
msy20020410 <dj8817117@gmail.com>
2025-09-09 14:19:49 +0800

    Convert类型转换工具

--- src/main/java/com/xm/testhutool/controller/convert/ConvertController.java
+++ src/main/java/com/xm/testhutool/controller/convert/ConvertController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @RestController
@@ -29,5 +30,25 @@ public class ConvertController {
     @GetMapping("/toDate")
     public Date toDate() {
         return Convert.toDate("2088-08-08");
+    }
+
+    // 将字符串转换为16进制的字符串
+    @GetMapping("/toHex")
+    public String toHex() {
+        String s = "你好,世界!";
+        return Convert.toHex(s, StandardCharsets.UTF_8);
+    }
+
+    // 将16进制的字符串转换为字符串
+    @GetMapping("/HexToStr")
+    public String toStrHex() {
+        String hex = "e4bda0e5a5bd2ce4b896e7958c21";
+        return Convert.hexToStr(hex,StandardCharsets.UTF_8);
+    }
+
+    // 金额大小写转换
+    @GetMapping("/toChinese")
+    public String toChinese() {
+        return Convert.digitToChinese(123456789.01);
     }
 }
